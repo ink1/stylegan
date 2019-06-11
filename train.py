a90e1379c7bbd455098ac74e67dfdae7f48c6896
unknown <i@k.org>
2019-06-11 22:34:09 +0100

    fix

--- train.py
+++ train.py
@@ -53,7 +53,7 @@ if 1:
     # Default options.
     train.total_kimg = 25000
     train.resume_run_id = 'latest'
-    train.resume_run_id = None
+    #train.resume_run_id = None
     sched.lod_initial_resolution = 8
     sched.G_lrate_dict = {128: 0.0015, 256: 0.002, 512: 0.003, 1024: 0.003}
     sched.D_lrate_dict = EasyDict(sched.G_lrate_dict)
